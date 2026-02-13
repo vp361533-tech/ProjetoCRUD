658a5d5bb98eaa0e2b8344c78d3882725c250117
vp361533-tech <vp361533@gmail.com>
2026-02-12 21:47:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about')
+def about_page():
+    a = 10
+    return f'<h1>Sombre {a}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
